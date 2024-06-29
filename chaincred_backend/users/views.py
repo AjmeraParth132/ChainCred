c3e1825484617e83d72ecea0336e24ab90e8557c
Manish <msn07082005@gmail.com>
2024-06-29 06:44:30 +0530

    Add: Signup And Login API

--- chaincred_backend/users/views.py
+++ chaincred_backend/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from .serializers import SignupSerializer, LoginSerializer
 
-# Create your views here.
+class SignupView(APIView):
+    """
+    View for the signup endpoint.
+    """
+    def post(self,request,*args,**kwargs):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self,request,*args,**kwargs):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(username=serializer.data['username'],password=serializer.data['password'])
+            if user is not None:
+                return Response({'message':'Login successful'},status=status.HTTP_200_OK)
+            return Response({'message':'Invalid credentials'},status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
